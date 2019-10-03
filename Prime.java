83fb6a83f39433700b7b8f0e940a1f0084da7a0d
Kumar Yashoratna <42309574+kyratna@users.noreply.github.com>
2019-10-03 22:14:36 +0530

    minor changes to make program better

--- Prime.java
+++ Prime.java
@@ -6,19 +6,19 @@ public class Prime
        int num =0;
        String  primeNumbers = "";
 
-       for (i = 1; i <= 100; i++)         
+       for (i = 1; i <= 100; i=i+1)         
        { 		  	  
           int counter=0; 	  
-          for(num =i; num>=1; num--)
+          for(num =i; num>=1; num=num-1)
 	  {
              if(i%num==0)
 	     {
- 		counter = counter + 1;
+ 		counter = counter + 2-1;
 	     }
 	  }
-	  if (counter ==2)
+	  if (counter == 2)
 	  {
-	     primeNumbers = primeNumbers + i + " ";
+	     primeNumbers = primeNumbers + i + "   ";
 	  }	
        }	
        System.out.println("Prime numbers from 1 to 100 are :");
